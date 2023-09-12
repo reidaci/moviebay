0b004322d9c3aea26147b0ed335090cd506e22f6
AlbianYzeiraj <albianyzeiraj27@gmail.com>
2023-09-12 15:11:03 +0200

    Login & SignUp form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login/login.component';
+import { SignUpComponent } from './features/sign-up/sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
--- src/app/features/sign-up/sign-up/sign-up.component.ts
+++ src/app/features/sign-up/sign-up/sign-up.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent {
+  formData = {
+    username: '',
+    password: '',
+  };
 
+  login() {
+    if (
+      this.formData.username === 'user' &&
+      this.formData.password === 'password'
+    ) {
+      console.log('Authentication successful');
+    } else {
+      console.log('Authentication failed');
+    }
+  }
 }
